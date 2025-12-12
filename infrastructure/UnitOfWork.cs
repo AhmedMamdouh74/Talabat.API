47a7d9e0954b62fda0971cf8ae2c4f38c6b8f63f
Ahmed Mamdouh <amamdouh823@gmail.com>
2025-12-12 19:38:05 +0200

    add unitOfWork

---
+++ infrastructure/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using Domain;
+using Domain.Contracts;
+using infrastructure.Repos;
+using System.Collections.Concurrent;
+
+namespace infrastructure
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext dbContext;
+        private ConcurrentDictionary<string, object> repositories = new ConcurrentDictionary<string, object>();
+
+
+
+        public UnitOfWork(AppDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity
+        {
+            var type = typeof(TEntity).Name;
+            if (!repositories.ContainsKey(type))
+            {
+                var repositoryInstance = new GenericRepository<TEntity>(dbContext);
+                repositories.TryAdd(type, repositoryInstance);
+            }
+            return (IGenericRepository<TEntity>)repositories[type];
+        }
+
+        public Task<int> CompleteAsync()
+        {
+            return dbContext.SaveChangesAsync();
+        }
+
+       
+
+        public async ValueTask DisposeAsync()
+        {
+           await dbContext.DisposeAsync();
+        }
+    }
+}
