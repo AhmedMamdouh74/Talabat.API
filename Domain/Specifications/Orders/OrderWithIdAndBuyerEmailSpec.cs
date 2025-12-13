555cc0359e60d1862753b4ced0fa8561968cf95d
Ahmed Mamdouh <amamdouh823@gmail.com>
2025-12-13 17:26:43 +0200

    add spec for order and finish implematation for OrderServcie()

---
+++ Domain/Specifications/Orders/OrderWithIdAndBuyerEmailSpec.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities.Order_Aggregate;
+
+namespace Domain.Specifications.Orders
+{
+    public class OrderWithIdAndBuyerEmailSpec : BaseSpecification<Order>
+    {
+        public OrderWithIdAndBuyerEmailSpec(int id, string buyerEmail) : base(o => o.Id == id && o.BuyerEmail == buyerEmail)
+        {
+            AddInclude(o => o.OrderItems);
+            AddInclude(o => o.DeliveryMethod);
+        }
+    }
+}
